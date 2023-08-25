5c4993740f2494c298e2c32a5692360ed5aaa059
Stu Cowley <stucowley@outlook.com>
2023-08-25 14:40:36 +0930

    Delete initial menu toggle script

--- src/js/scripts.js
+++
@@ -1,9 +0,0 @@
-const navToggle = document.querySelector('.header__menu-toggle');
-const headerNav = document.querySelector('#header-menu');
-
-navToggle.addEventListener('click', () => {
-  headerNav.hasAttribute('data-visible')
-    ? navToggle.setAttribute('aria-expanded', false)
-    : navToggle.setAttribute('aria-expanded', true);
-  headerNav.toggleAttribute('data-visible');
-});
