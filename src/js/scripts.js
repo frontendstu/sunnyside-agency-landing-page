e795f371aface32ddb91bcdaf7661a45b7f13696
Stu Cowley <stucowley@outlook.com>
2023-08-25 12:57:10 +0930

    Add header menu toggle script

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,9 @@
-console.log('Hello, World!');
+const navToggle = document.querySelector('.header__menu-toggle');
+const headerNav = document.querySelector('.header__menu');
+
+navToggle.addEventListener('click', () => {
+  headerNav.hasAttribute('data-visible')
+    ? navToggle.setAttribute('aria-expanded', false)
+    : navToggle.setAttribute('aria-expanded', true);
+  headerNav.toggleAttribute('data-visible');
+});
